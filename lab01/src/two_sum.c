471ef8dae9abec50d7c91f5c5df4bdd23c262cf3
Nicholas Nguyen <NicholasDNguyen291004@gmail.com>
2026-02-05 16:45:03 -0600

    Update two_sum.c

--- lab01/src/two_sum.c
+++ lab01/src/two_sum.c
@@ -8,7 +8,19 @@ int two_sum(const int* nums, int n, int target, int* out_i, int* out_j) {
     //  - store indices in *out_i and *out_j
     //  - return 1 if found; otherwise return 0
 
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (nums[i] + nums[j] == target) {
+                *out_i = i;
+                *out_j = j;
+                return 1;   // found a valid pair
+            }
+        }
+    }
+
     return 0;
 }
 
 //think about the time complexity and space complexity of your solution
+//I think it's O(n^2) for time complexity because I'm using two loops and O(1) for the space complexity!
+
