08839c4d15a815c05e9a0e3315f020bf0bcc641a
Nicholas Nguyen <NicholasDNguyen291004@gmail.com>
2026-03-08 18:11:30 -0500

    Update student.c

--- Assignment2/Question2/student.c
+++ Assignment2/Question2/student.c
@@ -27,12 +27,42 @@ struct TreeNode {
     struct TreeNode *left;
     struct TreeNode *right;
 };
+static int heightIfAVL(struct TreeNode* node, struct TreeNode* min, struct TreeNode* max) {
+    if (node == NULL) {
+        return 0;
+    }
+
+    if ((min != NULL && node->val <= min->val) ||
+        (max != NULL && node->val >= max->val)) {
+        return -1;
+    }
+
+    int leftHeight = heightIfAVL(node->left, min, node);
+    if (leftHeight == -1) {
+        return -1;
+    }
+
+    int rightHeight = heightIfAVL(node->right, node, max);
+    if (rightHeight == -1) {
+        return -1;
+    }
+
+    int diff = leftHeight - rightHeight;
+    if (diff < -1 || diff > 1) {
+        return -1;
+    }
+
+    if (leftHeight > rightHeight) {
+        return leftHeight + 1;
+    } else {
+        return rightHeight + 1;
+    }
+}
 
 bool isAVL(struct TreeNode* root) {
     // TODO: implement
     // Hint: One common O(n) approach:
     // - Use a recursive helper that returns the subtree height,
     //   and returns -1 if subtree is invalid (BST violation or unbalanced).
-    (void)root;
-    return false;
+    return heightIfAVL(root, NULL, NULL) != -1;
 }
