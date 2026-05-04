17f65308be784dffaeaf5ab0537059d6407a8d56
Nicholas Nguyen <NicholasDNguyen291004@gmail.com>
2026-05-04 14:21:53 -0500

    Update student.c

--- lab07/Question2/student.c
+++ lab07/Question2/student.c
@@ -1,3 +1,5 @@
+#include <stdbool.h>
+
 /*
 Question 2: Merge Sort
 
@@ -28,9 +30,68 @@ Notes:
 - You may use temporary arrays inside your merge function.
 */
 
-void mergeSort(int arr[], int size) {
-    // TODO: implement merge sort
-    (void)arr;
-    (void)size;
+void merge(int arr[], int left, int mid, int right) {
+    int leftSize = mid - left + 1;
+    int rightSize = right - mid;
+
+    int leftArray[leftSize];
+    int rightArray[rightSize];
+
+    for (int i = 0; i < leftSize; i++) {
+        leftArray[i] = arr[left + i];
+    }
+
+    for (int j = 0; j < rightSize; j++) {
+        rightArray[j] = arr[mid + 1 + j];
+    }
+
+    int i = 0;
+    int j = 0;
+    int k = left;
+
+    while (i < leftSize && j < rightSize) {
+        if (leftArray[i] <= rightArray[j]) {
+            arr[k] = leftArray[i];
+            i++;
+        } else {
+            arr[k] = rightArray[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < leftSize) {
+        arr[k] = leftArray[i];
+        i++;
+        k++;
+    }
+
+    while (j < rightSize) {
+        arr[k] = rightArray[j];
+        j++;
+        k++;
+    }
 }
 
+void mergeSortHelper(int arr[], int left, int right) {
+    if (left >= right) {
+        return;
+    }
+
+    int mid = left + (right - left) / 2;
+
+    mergeSortHelper(arr, left, mid);
+    mergeSortHelper(arr, mid + 1, right);
+
+    merge(arr, left, mid, right);
+}
+
+void mergeSort(int arr[], int size) {
+    // If the array is empty or has only one element, do nothing
+    if (size <= 1) {
+        return;
+    }
+
+    // Start merge sort from index 0 to index size - 1
+    mergeSortHelper(arr, 0, size - 1);
+}
